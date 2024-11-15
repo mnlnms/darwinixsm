e50c0589eae7a2ee2a80c7e7bf052734b8e48a09
manu <>
2024-11-15 21:20:14 +0100

    Formating, removing incus

--- flake.nix
+++ flake.nix
@@ -22,145 +22,144 @@
           [ 
           ];
 	
-	environment.etc = {
-	  "pam.d/sudo_local".text = ''
-	    # Managed by Nix-Darwin
-	    auth	optional	${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
-	    auth	sufficient	pam_tid.so
-	  '';
-	};
-	homebrew = {
-          enable = true;
-  	brews = [
-	  # Installing & updating MacOS AppStore Packages via nix-darwin
-  	  "mas"
-	  # terminal / cli
-	  "fd"
-	  "fzf"
-	  "oh-my-posh"
-	  "ripgrep"
-	  "tmux"
-	  "yazi"
-	  # yazi prerequisition
-	  "ffmpegthumbnailer"
-	  "imagemagick"
-	  "poppler"
-	  "sevenzip"
-	  "xclip"
-	  # end
-	  "zellij"
-	  "zoxide"
-	  # Dotfile - Management
-	  "chezmoi"
-	  # Editors
-	  "neovide"
-	  "neovim"
-	  # Other CLI - Tools
-	  "bitwarden-cli"
-	  # Virtual Machines / Dev Containers
-	  "colima"
-	  "docker"
-	  "incus"
-  	];
-  	casks = [
-	  # Work
-  	  "microsoft-edge"
-	  "microsoft-teams"
-	  "webex"
-	  "powershell"
-	  # Editors
-	  "visual-studio-code"
-	  # Private
-  	  "iina"
-	  "spotify"
-	  "firefox"
-	  "winbox"
-	  # System
-      "alacritty"
-	  "betterdisplay"
-  	  "raycast"
-	  "wezterm"
-	  "zerotier-one"
-	  # Virtualization
-	  "vmware-fusion"
-  	];
-  	masApps = {
-          # Microsoft Office
-          "Microsoft Word" = 462054704;
-  	  "Microsoft Outlook" = 985367838;
-	  # Safari Extensions
-  	  "Bitwarden - Safari" = 1352778147;
-  	  "AdGuard für Safari" = 1440147259;
-  	};
-  	onActivation.cleanup = "zap";
-  	onActivation.autoUpdate = true;
-  	onActivation.upgrade = true;
-        };
-	
-        networking = {
-          hostName = "mcbk-air.nemes.cc";
-  	localHostName = "mcbk-air";
-  	computerName = "mcbk-air";
-        };
-  
-        security.pam.enableSudoTouchIdAuth = true;
-  
-        system = {
-          defaults = {
-            dock = {
-	      autohide = true;
-	      persistent-apps = [
-  	        "/Applications/Safari.app"
-  	        "/System/Applications/Mail.app"
-  	        "/System/Applications/Calendar.app"
-  	      ];
-              orientation = "left";
-              show-recents = false;
-              tilesize = 32;
-	    };
-  	    finder.FXPreferredViewStyle = "clmv";
-  	    loginwindow.GuestEnabled = false;
-  	    trackpad = {
-	      Clicking = true;
-  	      Dragging = true;
-  	      TrackpadThreeFingerDrag = true;
-  	    };
-	    NSGlobalDomain = {
-	      AppleICUForce24HourTime = true;
-  	      AppleInterfaceStyle = "Dark";
-  	      KeyRepeat = 2;
-	    };
-          };
-  	  startup.chime = false;
-        };
-  
-        users.users.mnl.home = "/Users/mnl";
+				environment.etc = {
+					"pam.d/sudo_local".text = ''
+						# Managed by Nix-Darwin
+						auth	optional	${pkgs.pam-reattach}/lib/pam/pam_reattach.so ignore_ssh
+						auth	sufficient	pam_tid.so
+					'';
+				};
+				homebrew = {
+					enable = true;
+					brews = [
+						# Installing & updating MacOS AppStore Packages via nix-darwin
+						"mas"
+						# terminal / cli
+						"fd"
+						"fzf"
+						"oh-my-posh"
+						"ripgrep"
+						"tmux"
+						"yazi"
+						# yazi prerequisition
+						"ffmpegthumbnailer"
+						"imagemagick"
+						"poppler"
+						"sevenzip"
+						"xclip"
+						# end
+						"zellij"
+						"zoxide"
+						# Dotfile - Management
+						"chezmoi"
+						# Editors
+						"neovide"
+						"neovim"
+						# Other CLI - Tools
+						"bitwarden-cli"
+						# Virtual Machines / Dev Containers
+						"colima"
+						"docker"
+					];
+					casks = [
+						# Work
+						"microsoft-edge"
+						"microsoft-teams"
+						"webex"
+						"powershell"
+						# Editors
+						"visual-studio-code"
+						# Private
+						"iina"
+						"spotify"
+						"firefox"
+						"winbox"
+						# System
+						"alacritty"
+						"betterdisplay"
+						"raycast"
+						"wezterm"
+						"zerotier-one"
+						# Virtualization
+						"vmware-fusion"
+					];
+					masApps = {
+						# Microsoft Office
+						"Microsoft Word" = 462054704;
+						"Microsoft Outlook" = 985367838;
+						# Safari Extensions
+						"Bitwarden - Safari" = 1352778147;
+						"AdGuard für Safari" = 1440147259;
+					};
+					onActivation.cleanup = "zap";
+					onActivation.autoUpdate = true;
+					onActivation.upgrade = true;
+				};
+				
+				networking = {
+				hostName = "mcbk-air.nemes.cc";
+				localHostName = "mcbk-air";
+				computerName = "mcbk-air";
+				};
+				
+				security.pam.enableSudoTouchIdAuth = true;
+				
+				system = {
+					defaults = {
+						dock = {
+							autohide = true;
+							persistent-apps = [
+								"/Applications/Safari.app"
+								"/System/Applications/Mail.app"
+								"/System/Applications/Calendar.app"
+							];
+							orientation = "left";
+							show-recents = false;
+							tilesize = 32;
+						};
+						finder.FXPreferredViewStyle = "clmv";
+						loginwindow.GuestEnabled = false;
+						trackpad = {
+							Clicking = true;
+							Dragging = true;
+							TrackpadThreeFingerDrag = true;
+						};
+						NSGlobalDomain = {
+							AppleICUForce24HourTime = true;
+							AppleInterfaceStyle = "Dark";
+							KeyRepeat = 2;
+						};
+					};
+					startup.chime = false;
+				};
+				
+				users.users.mnl.home = "/Users/mnl";
 
-	fonts.packages = with pkgs; [
-	 (nerdfonts.override { fonts = ["FiraCode" "JetBrainsMono"]; })
-	];
-  
-        # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
-        # nix.package = pkgs.nix;
-  
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-  
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true;  # default shell on catalina
-        # programs.fish.enable = true;
-  
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-  
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 5;
-  
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
+				fonts.packages = with pkgs; [
+					(nerdfonts.override { fonts = ["FiraCode" "JetBrainsMono"]; })
+				];
+				
+				# Auto upgrade nix package and the daemon service.
+				services.nix-daemon.enable = true;
+				# nix.package = pkgs.nix;
+
+				# Necessary for using flakes on this system.
+				nix.settings.experimental-features = "nix-command flakes";
+
+				# Create /etc/zshrc that loads the nix-darwin environment.
+				programs.zsh.enable = true;  # default shell on catalina
+				# programs.fish.enable = true;
+
+				# Set Git commit hash for darwin-version.
+				system.configurationRevision = self.rev or self.dirtyRev or null;
+
+				# Used for backwards compatibility, please read the changelog before changing.
+				# $ darwin-rebuild changelog
+				system.stateVersion = 5;
+
+				# The platform the configuration will be used on.
+				nixpkgs.hostPlatform = "aarch64-darwin";
+			};
 
     in
     {
@@ -170,16 +169,15 @@
         # specialArgs = { inherit inputs; };
         modules = [ 
           configuration
-	  ./configuration.nix
-  	  nix-homebrew.darwinModules.nix-homebrew
-  	  {
-  	    nix-homebrew =
-  	    {
-  	      enable = true;
-  	      enableRosetta = true;
-  	      user = "mnl";
-  	    };
-  	  }
+	  			./configuration.nix
+  	  		nix-homebrew.darwinModules.nix-homebrew
+  	  		{
+  	    		nix-homebrew = {
+  	      		enable = true;
+  	      		enableRosetta = true;
+  	      		user = "mnl";
+  	   			};
+  	  		}
         ];
       };
   
@@ -187,4 +185,3 @@
       darwinPackages = self.darwinConfigurations."air".pkgs;
     };
 }
-
