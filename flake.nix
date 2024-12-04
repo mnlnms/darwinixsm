425bd2fb7db17c26ccb7e998f275c6b11871601d
mws-km <m.kostelecky@mwservice.at>
2024-12-04 07:53:46 +0100

    Switching to 24.11-darwin

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Darwinixsm - My journey of nixological evolution";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
