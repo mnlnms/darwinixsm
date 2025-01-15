515e9245bfb532ed305e64691c746e2141031241
mws-km <m.kostelecky@mwservice.at>
2025-01-15 21:10:39 +0100

    Pinning nix-darwin and nixpkgs to 24.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Darwinixsm - My journey of nixological evolution";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
